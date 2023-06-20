90ecfcdc8a2ba41785a1eda74cc8bcd61d50ca07
xKonZi <54500722+xKonZi@users.noreply.github.com>
2023-06-20 13:18:21 +0200

    Organized files and folders

---
+++ CLIENT/ClientProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
